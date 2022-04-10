a764a2a6340c625daf25ed51bb530b9c25a50588
Eivind Næss <eivnaes@yahoo.com>
2022-04-10 14:06:09 -0700

    nm-sstp-service.c should extend copyright to same as the global copyrights in debian/copyright
    Signed-off-by: Eivind Næss <eivnaes@yahoo.com>

--- src/nm-sstp-service.c
+++ src/nm-sstp-service.c
@@ -17,7 +17,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * (C) Copyright 2008 - 2014 Red Hat, Inc.
+ * (C) Copyright 2008 - 2018 Red Hat, Inc.
  */
 
 #include "nm-default.h"
